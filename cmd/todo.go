19c29e714aaacdcb59e6bbcde49c3876c0e3c12a
Peter Krantz <zyrree@gmail.com>
2021-12-27 21:17:56 +0100

    added todo command and basic styling

---
+++ cmd/todo.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+	"github.com/zyrre/next/utils"
+)
+
+// todoCmd represents the todo command
+var todoCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fileMap, _ := utils.FileToMap("next.md")
+		for _, todo := range fileMap["To do"] {
+			fmt.Println(chalk.Italic.TextStyle(todo))
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(todoCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// todoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// todoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
