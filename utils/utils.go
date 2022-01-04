8605726aaac94f5f1006b76a1fe2ca4201b34059
Peter Krantz <zyrree@gmail.com>
2022-01-04 20:31:02 +0100

    added starting new task from todo list

--- utils/utils.go
+++ utils/utils.go
@@ -108,6 +108,29 @@ func RemoveTask(task string) error {
 	return nil
 }
 
+func StartTask(task string) error {
+	lines, doingStart, _, err := parseFile(filename)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return err
+	}
+	defer file.Close()
+	for i, line := range lines {
+		if doingStart != 0 && i == doingStart+2 {
+			fmt.Fprintln(file, "- "+task)
+		}
+		if i > doingStart || strings.Trim(line, "- ~") != task {
+			fmt.Fprintln(file, line)
+		}
+	}
+	return nil
+}
+
 func mdDataToMap(mdData []string) map[string][]string {
 	mdMap := make(map[string][]string)
 	var currentIndex string
