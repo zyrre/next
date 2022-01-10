d2140f0e08e90bc1308f40ed19a6aa5b5e157f04
Peter Krantz <zyrree@gmail.com>
2022-01-10 23:43:31 +0100

    added reuse window flag for code launch

--- cmd/start.go
+++ cmd/start.go
@@ -24,7 +24,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		utils.StartTask(args[0])
-		_ = exec.Command("code", ".").Run()
+		_ = exec.Command("code", ".", "-r").Run()
 		fmt.Println(args[0] + " started")
 	},
 }
